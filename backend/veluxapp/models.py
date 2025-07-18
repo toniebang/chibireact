1b8669916320b7191450a15150fb8bbfb6e19c68
toniebang <toni0097.97@gmail.com>
2025-07-18 23:05:59 +0100

    Modifico customuser

--- backend/veluxapp/models.py
+++ backend/veluxapp/models.py
@@ -14,23 +14,23 @@ class CustomUser(AbstractUser):
     profile_picture = models.URLField(max_length=500, null=True, blank=True)
     google_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions '
-                  'granted to each of their groups.',
-        related_name="custom_user_set",
-        related_query_name="custom_user",
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name="custom_user_set",
-        related_query_name="custom_user",
-    )
+    # groups = models.ManyToManyField(
+    #     'auth.Group',
+    #     verbose_name='groups',
+    #     blank=True,
+    #     help_text='The groups this user belongs to. A user will get all permissions '
+    #               'granted to each of their groups.',
+    #     related_name="custom_user_set",
+    #     related_query_name="custom_user",
+    # )
+    # user_permissions = models.ManyToManyField(
+    #     'auth.Permission',
+    #     verbose_name='user permissions',
+    #     blank=True,
+    #     help_text='Specific permissions for this user.',
+    #     related_name="custom_user_set",
+    #     related_query_name="custom_user",
+    # )
 
 
 # ------------------- Categoria --------------------------
