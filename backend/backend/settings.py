5a3f9272a086332ae4465111b87ae8ff5e61c023
toniebang <toni0097.97@gmail.com>
2025-08-08 14:13:16 +0100

    tr

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -238,7 +238,7 @@ else:
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'veluxapp/static'),
+        os.path.join(BASE_DIR, 'static'),
     ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
