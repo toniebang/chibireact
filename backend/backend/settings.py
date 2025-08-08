252356e77067673a6ba4a1124b88fc928cb30b88
toniebang <toni0097.97@gmail.com>
2025-08-08 15:08:15 +0100

    df

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -236,10 +236,10 @@ else:
     
     
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
